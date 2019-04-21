cf3e29095045619282fd228370d5d3ebe34b126a
nikolausn <nikolaus.nova@gmail.com>
2019-04-21 00:40:52 -0500

    add refine client sub module

--- script/run_history.py
+++ script/run_history.py
@@ -13,3 +13,11 @@ def run_history(input_file,history_dict,output_file):
     """
 
     raise NotImplementedError
+
+
+
+if __name__ == '__main__':
+    from script.extract_archive_history import extract_history
+    project_file = "../test_files/airbnb_test.tar.gz"
+    recipe = extract_history(project_file)
+    
